7cd26d2d45fd940f4ffca934b47fe416e742b90c
DanielFreire <dfgs.fish@gmail.com>
2017-08-12 19:21:40 -0400

    Criação da Toolbar

--- app/src/main/java/dfsolutions/com/tonnerdelivery/activity/MainActivity.java
+++ app/src/main/java/dfsolutions/com/tonnerdelivery/activity/MainActivity.java
@@ -3,6 +3,10 @@ package dfsolutions.com.tonnerdelivery.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,12 +14,14 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.zip.Inflater;
+
 import dfsolutions.com.tonnerdelivery.R;
 import dfsolutions.com.tonnerdelivery.config.ConfiguracaoFirebase;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button botaoSair;
+    private Toolbar toolbar;
     private FirebaseAuth autenticacao;
 
     @Override
@@ -23,21 +29,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        botaoSair = (Button) findViewById(R.id.botao_sair_id);
+        autenticacao = ConfiguracaoFirebase.getAutenticacaoFirebase();
 
-        botaoSair.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        toolbar = (Toolbar) findViewById(R.id.toolbar_id);
+        toolbar.setTitle(R.string.app_name);
+        setSupportActionBar(toolbar);
 
-                autenticacao = ConfiguracaoFirebase.getAutenticacaoFirebase();
-                autenticacao.signOut();
+    }
 
-                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
 
-            }
-        });
+        MenuInflater inflater = getMenuInflater(); //Retorna um objeto MenuInflater com o contexto da aplicação
+        inflater.inflate(R.menu.menu_main, menu);
 
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.action_sair:
+                deslogarUsuario();
+                return true;
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+
+    }
+
+    public void deslogarUsuario(){
+        autenticacao.signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
